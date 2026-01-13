cea3666342944db76f0fa7aad87b131262623ad7
Lucas <lucasadeodato3@gmail.com>
2026-01-12 21:15:57 -0300

    feat: add more sample rays per pixel to get antialiasing

--- main.cpp
+++ main.cpp
@@ -9,14 +9,13 @@ int main() {
 	hittable_list world;
 
 	world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
-	world.add(std::make_shared<sphere>(point3(0, -100.5, -150), 100));
+	world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100));
 
 	camera cam;
 
 	cam.aspect_ratio = 16.0 / 9.0;
 	cam.image_width = 400;
+	cam.samples_per_pixel = 100;
 
 	cam.render(world);
-	
-
 }
---
+++ utils.h
@@ -0,0 +1,19 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <random>
+namespace utils {
+
+	inline double random_double() {
+		static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+		static std::mt19937 generator;
+		return distribution(generator);
+	}
+
+	inline double random_double(double min, double max) {
+		return min + (max - min) * random_double();
+	}
+
+}
+
+#endif
