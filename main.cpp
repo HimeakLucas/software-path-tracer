1caafa76008fa592fb2ddd6b70531b54c81ca5a2
Lucas <lucasadeodato3@gmail.com>
2026-07-28 23:08:15 -0300

    chore: update presets

--- main.cpp
+++ main.cpp
@@ -20,30 +20,26 @@ int main(int argc, char* argv[]) {
 	std::string set_camera = argv[2];
 
 	if (set_camera == "0") {
-		cam.aspect_ratio = 16.0 / 9.0;
 		cam.image_width = 400;
 		cam.samples_per_pixel = 100;
-		cam.max_depth = 50;
+		cam.max_depth = 3;
 
 	
 	} else if (set_camera == "1") {
-		cam.aspect_ratio = 16.0 / 9.0;
 		cam.image_width = 900;
 		cam.samples_per_pixel = 900;
-		cam.max_depth = 50;
+		cam.max_depth = 8;
 
 	} else if (set_camera == "2") {
-		cam.aspect_ratio = 16.0 / 9.0;
-		cam.image_width = 1080;
+		cam.image_width = 1280;
 		cam.samples_per_pixel = 1500;
-		cam.max_depth = 50;
+		cam.max_depth = 8;
 
 
 	} else if (set_camera == "3") {
-		cam.aspect_ratio = 16.0 / 9.0;
 		cam.image_width = 1920;
-		cam.samples_per_pixel = 15000;
-		cam.max_depth = 50;
+		cam.samples_per_pixel = 10000;
+		cam.max_depth = 10;
 	}
 
 	cam.initialize();
